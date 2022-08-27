e738503f1c8e07afb3b241063f54b4058e25efd5
JohnIdenyi <johnidenyi2001@gmail.com>
2022-08-26 21:59:03 -0400

    updated data file

--- src/data.js
+++ src/data.js
@@ -1,16 +1,26 @@
 export const data = [
     {
         id: 1,
-        title: "Mount Fuji",
-        location: "Japan",
+        title: "Magic Land",
+        location: "Abuja",
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
+        description: "Magic Land, also known as wonderland is a good park to be in alone, with a loved one, or with family. It has all you need to plan the perfect date or outing.",
+        imageUrl: "https://i0.wp.com/egtechblog.com/wp-content/uploads/2022/06/carousel-horses-carnival-merry-go-round-1.jpg?resize=800%2C445&ssl=1"
+    },
+    {
+        id: 2,
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Dec, 2022",
+        endDate: "22 Jan, 2021",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
         imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
     },
     {
-        id: 2,
+        id: 3,
         title: "Sydney Opera House",
         location: "Australia",
         googleMapsUrl: "https://www.google.com/maps/place/Sydney+Opera+House/@-33.8567799,151.213108,17z/data=!3m2!4b1!5s0x6b12ae67d234a27f:0xd6d4e9380ca1e32f!4m5!3m4!1s0x6b12ae665e892fdd:0x3133f8d75a1ac251!8m2!3d-33.8567844!4d151.2152967",
@@ -20,7 +30,7 @@ export const data = [
         imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
     },
     {
-        id: 3,
+        id: 4,
         title: "Geirangerfjord",
         location: "Norway",
         googleMapsUrl: "https://www.google.com.ng/maps/place/Geirangerfjord/@62.1048487,7.0752131,12z/data=!3m1!4b1!4m5!3m4!1s0x46169d427b268c51:0xb8c99540dcc397fe!8m2!3d62.101506!4d7.0940817?hl=enhttps://goo.gl/maps/1DGM5WrWnATgkSNB8",
